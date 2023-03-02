d51aa493dd5de68277eb6dc3cca255876e9b6975
JavadAli <javadalivpk45@gmail.com>
2023-03-03 04:04:44 +0530

    first commit

---
+++ simpleCalc.js
@@ -0,0 +1,41 @@
+const question="What is  60 divided  by 5?"
+let data=question.split(" ");
+let values=[];
+let operator;
+for(let i of data){
+    let value=parseInt(i);
+    if(value){
+       values.push(value)
+    }
+
+    if(i==="plus" || i==="minus" || i==="multiplied" || i==="divided" ){
+        operator=i;
+    }
+}
+
+let num1=values[0];
+let num2=values[1];
+
+let result;
+    switch (operator) {
+      case 'plus':
+        result = num1 + num2;
+        break;
+      case 'minus':
+        result = num1 - num2;
+        break;
+      case 'multiplied':
+        result = num1 * num2;
+        break;
+      case 'divided':
+        result = num1 / num2;
+        break;
+      default:
+        result=undefined;
+       
+    }
+    if(result){
+        console.log(`${num1} ${operator} ${num2} is ${result}`);
+    }else{
+        console.log("invalid");
+    }
